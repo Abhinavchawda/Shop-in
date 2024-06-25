207edc4d56560b56572453d4a1f6655185878c9f
Abhinav Chawda <123491911+Abhinavchawda@users.noreply.github.com>
2024-06-25 22:45:33 +0530

    Delete src/app directory

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import productReducer from '../features/ProductList/ProductSlice';
-
-export const store = configureStore({
-  reducer: {
-    product: productReducer,
-  },
-});
